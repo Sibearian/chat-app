3fb858380f3b4ffe8812d6278a532d97e2617bf9
srujan <srujansarode@protonmail.com>
2021-06-07 15:08:01 +0530

    linked firebase to my app

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const config = {
+	apiKey: "AIzaSyAbt8iiZ-VWL2xa4Z-YpTGl0fRMINbNeC8",
+	authDomain: "chat-web-app-97b27.firebaseapp.com",
+	projectId: "chat-web-app-97b27",
+	storageBucket: "chat-web-app-97b27.appspot.com",
+	messagingSenderId: "394698411529",
+	appId: "1:394698411529:web:127ccf36d104265cf5f2d8",
+};
+
+const app = firebase.initializeApp(config);
